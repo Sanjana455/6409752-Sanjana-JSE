24d18f2e94683f226dc7b78851b9ef1de06bd35a
Sanjana455 <sanjanahazari4@gmail.com>
2025-07-06 00:53:37 +0530

    Create UserService.java

---
+++ TDD using JUnit5 and Mockito/Mockito_Exercises/Mocking_and_Stubbing/src/main/java/UserService.java
@@ -0,0 +1,12 @@
+public class UserService {
+    private final UserRepository repo;
+
+    public UserService(UserRepository repo) {
+        this.repo = repo;
+    }
+
+    public String getGreeting(int id) {
+        String name = repo.findNameById(id);
+        return "Welcome, " + name;
+    }
+}
