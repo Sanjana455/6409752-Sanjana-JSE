aba369c23f968e46200e6d83f2ef7093f1b020b9
Sanjana455 <sanjanahazari4@gmail.com>
2025-07-07 21:50:31 +0530

    Create Main.java

---
+++ Spring_Core_and_Maven/Exercise-1_Configuring_a_basic_Spring_Application/code/week-3_Exercise-1/src/main/java/com/college/Main.java
@@ -0,0 +1,16 @@
+package com.college;
+
+import com.college.service.StudentService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        StudentService studentService = context.getBean("studentService", StudentService.class);
+
+        studentService.registerStudent("Sanjana Reddy");
+    }
+}
