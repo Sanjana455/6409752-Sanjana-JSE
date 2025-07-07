0088648bf71187de29225cf506b4d4dd8367521e
Sanjana455 <sanjanahazari4@gmail.com>
2025-07-07 23:44:10 +0530

    Create Application.java

---
+++ SLF4J_Logging_Exercises/Logging_Error_Messages_and_Warning_Levels/src/main/java/com/example/logging/Application.java
@@ -0,0 +1,9 @@
+package com.example.logging;
+
+public class Application {
+    public static void main(String[] args) {
+        NumberUtils.isPrime(1);   
+        NumberUtils.isPrime(4);  
+        NumberUtils.isPrime(7);  
+    }
+}
