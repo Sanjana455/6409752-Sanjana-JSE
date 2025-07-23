4129fff59efa78d2e2c172d2d70ecc89d34b8aff
Sanjana455 <sanjanahazari4@gmail.com>
2025-07-23 21:10:46 +0530

    Create CountryApiApplication.java

---
+++ 4_Spring_REST_using_Spring_Boot_3/spring_rest_handson/REST - Country_Web_Service/code/src/main/com/example/countryapi/CountryApiApplication.java
@@ -0,0 +1,11 @@
+package com.example.countryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CountryApiApplication.class, args);
+    }
+}
