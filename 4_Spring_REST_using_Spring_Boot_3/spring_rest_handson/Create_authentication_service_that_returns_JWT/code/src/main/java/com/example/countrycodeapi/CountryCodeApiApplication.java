8aaf054ee8d17bdfe40809eb282e39b8f4db799a
Sanjana455 <sanjanahazari4@gmail.com>
2025-07-23 21:26:41 +0530

    Create CountryCodeApiApplication.java

---
+++ 4_Spring_REST_using_Spring_Boot_3/spring_rest_handson/Create_authentication_service_that_returns_JWT/code/src/main/java/com/example/countrycodeapi/CountryCodeApiApplication.java
@@ -0,0 +1,11 @@
+package com.example.countrycodeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryCodeApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CountryCodeApiApplication.class, args);
+    }
+}
