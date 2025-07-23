1131cd3ebaa5b4904e2a4a792e97021fcd4290de
Sanjana455 <sanjanahazari4@gmail.com>
2025-07-23 20:16:21 +0530

    Create MainApp.java

---
+++ 4_Spring_REST_using_Spring_Boot_3/Spring_rest_handson/Spring_Core – Load_Country_from_Spring_Configuration_XML/src/com/example/main/MainApp.java
@@ -0,0 +1,13 @@
+package com.example.main;
+
+import com.example.model.Country;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("com/example/config/applicationContext.xml");
+        Country country = (Country) context.getBean("country");
+        System.out.println(country);
+    }
+}
