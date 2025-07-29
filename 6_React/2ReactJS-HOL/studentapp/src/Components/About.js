4ca56e89b7b92f8372e01fd5efdc5ee106ba7af8
Sanjana455 <sanjanahazari4@gmail.com>
2025-07-29 22:13:07 +0530

    Added studentapp React project under 2ReactJS-HOL

---
+++ 6_React/2ReactJS-HOL/studentapp/src/Components/About.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const About = () => {
+  return <h2>Welcome to the About page of the Student Management Portal</h2>;
+};
+
+export default About;
---
+++ 6_React/2ReactJS-HOL/studentapp/src/Components/Contact.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Contact = () => {
+  return <h2>Welcome to the Contact page of the Student Management Portal</h2>;
+};
+
+export default Contact;
